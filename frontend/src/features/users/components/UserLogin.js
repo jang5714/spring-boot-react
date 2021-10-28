2cb72309442c353d027f7ccb4dcb4f738112ef2c
jang5714 <lionx3@naver.com>
2021-10-28 20:09:20 +0900

    1026

--- frontend/src/features/users/components/UserLogin.js
+++ frontend/src/features/users/components/UserLogin.js
@@ -21,22 +21,22 @@ export default function UserLogin() {
     e.preventDefault()
     e.stopPropagation()
     const loginRequest = {username, password}
-    // userLogin(loginRequest)
-    // .then(res => {
-    //   const user = res.data;
-    //   if(user.username != null){
-    //     alert('로그인 성공, '+JSON.stringify(res.data))
-    //     localStorage.setItem('sessionUser', JSON.stringify(res.data))
-    //     history.push("/users/detail")
-    //   }else{
-    //     alert('아이디, 비번 오류로 로그인 실패  ')
-    //     changeNull(['username','password'])
-    //   }
-    // })
-    // .catch(err => {
-    //   alert('접속 실패' + err)
-    //   changeNull(['username','password'])
-    // })
+    userLogin(loginRequest)
+    .then(res => {
+      const user = res.data;
+      if(user.username != null){
+        alert('로그인 성공, '+JSON.stringify(res.data))
+        localStorage.setItem('sessionUser', JSON.stringify(res.data))
+        history.push("/users/detail")
+      }else{
+        alert('아이디, 비번 오류로 로그인 실패  ')
+        changeNull(['username','password'])
+      }
+    })
+    .catch(err => {
+      alert('접속 실패' + err)
+      changeNull(['username','password'])
+    })
 
   }
   

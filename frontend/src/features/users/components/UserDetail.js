763002b976dfe915c948ea16661c6e60d385dc41
jang5714 <lionx3@naver.com>
2021-10-30 18:02:56 +0900

    1030

--- frontend/src/features/users/components/UserDetail.js
+++ frontend/src/features/users/components/UserDetail.js
@@ -1,43 +1,48 @@
-import React, { useState, useEffect, useCallback} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 import { useDispatch } from 'react-redux';
-import { Link, useHistory } from 'react-router-dom';
-import { loginPage, logoout } from '../reducer/userSlice';
-
-
 
 export default function UserDetail() {
+    const history = useHistory()
     const detail = JSON.parse(localStorage.getItem('sessionUser'))
     const dispatch = useDispatch()
-    const history = useHistory()
-
   return (
     <div>
-        <h1>회원 정보</h1>
+         <h1>회원정보</h1>
         <ul>
             <li>
                 <label>
-                    <span>아이디: {detail.userId}</span>
+                    <span>회원번호 : {detail.userId} </span>
                 </label>
             </li>
             <li>
                 <label>
-                <span>email: {detail.email}</span>
+                    <span>아이디 : {detail.username} </span>
                 </label>
             </li>
             <li>
                 <label>
-                <span>비밀번호: *******</span>
+                <span>이메일 :  {detail.email}  </span>
                 </label>
             </li>
             <li>
                 <label>
-                <span>이름: {detail.name}</span>
+                    <span>비밀 번호 :  *******  </span>
                 </label>
             </li>
+            <li>
+                <label>
+                <span>이름 : {detail.name} </span>
+                </label>
+            </li>
+           
             <li>
                 <input type="button" value="회원정보수정" onClick={()=> history.push('/users/modify')}/>
-                <input type="button" value="로그아웃" onClick={dispatch(logoout)}/>
             </li>
+            {/* <li>
+                <input type="button" value="로그아웃" onClick={dispatch(logoout)}/>
+            </li> */}
         </ul>
     </div>
   );
--- frontend/src/features/users/components/UserLogin.js
+++ frontend/src/features/users/components/UserLogin.js
@@ -1,85 +1,53 @@
 import React, { useState, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import { useHistory  } from 'react-router-dom';
 import { loginPage } from 'features/users/reducer/userSlice'
-import {useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 import styled from 'styled-components'
 
 export default function UserLogin() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const [login, setLogin] = useState({})
-  const {username, password} = login
-  const history = useHistory()
   const dispatch = useDispatch()
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-  const handleChange = useCallback (
-    e => {
-    const {value, name} = e.target
-    setLogin({
-      ...login,
-      [name] : value
-    })
-  }, [login]
-  )
-  const changeNull = ls =>{
-    for(const i of ls ){
-      document.getElementById(i).value = ''
-    }
-  }
-
-  const onSubmit = async(e) => {
-    const loginRequest = {username, password}
-    await dispatch(loginPage(loginRequest))
-    const loginUser = JSON.parse(localStorage.getItem('sessionUser'))
-    if(loginUser.username != null){
-      alert(`${loginUser.name}님 환영합니다.`)
-      history.push("/users/detail")
-    }else{
-      alert('아이디, 비번 오류로 로그인 실패  ')
-      changeNull(['username','password'])
-    }
-  }
-  
   return (
     <div>
-      <form method="POST" onSubmit={handleSubmit(onSubmit)}>
-      <ul>
-          <li>
-            <label for="id">아이디
-              <input type="text" id="username" name='username' value={username} 
-                aria-invalid={errors.name ? "true" : "false"}
-                {...register('username', {required: true, maxlength:30})}
-                onChange={handleChange}/>
-            </label>
-          </li>
-          <small> 
-              {errors.password && errors.password.type === "required" && <Span>아이디를 입력해 주세요</Span>}
-              {errors.password && errors.password.type === "maxLength" && <Span>Max length exceeded</Span>}
-            </small>
-          <li>
-            <label for="pw">비밀번호
-            </label>
-                <input type="password" id="password" name="password"
-                aria-invalid={errors.name ? "true" : "false"}
-                {...register('password', {required: true, maxlength:30})}
-                onChange={handleChange}/>
+         <h1>로그인</h1>
+    <form method='POST' 
+    onSubmit={ 
+      handleSubmit(async (data) => {await dispatch(loginPage(data))})}>
+        <ul>
+            <li>
+                <label>아이디 : </label>
+                <input type="text" id="username" 
+                    {...register('username', { required: true, maxLength: 30 })}
+                    size="10" minlength="1" maxlength="15"/>
+                    {errors.username && errors.username.type === "required" && (
+                        <Span role="alert">아이디는 필수값입니다</Span>
+                    )}
+                    {errors.username && errors.username.type === "maxLength" && (
+                        <Span role="alert">아이디는 4자에서 15 글자이어야 합니다.</Span>
+                    )}
+                <br/>
+                <small>4~15자리 이내의 영문과 숫자</small>
             </li>
-            <li >
-            <small> 
-              {errors.password && errors.password.type === "required" && <Span>비밀번호를 입력해 주세요</Span>}
-              {errors.password && errors.password.type === "maxLength" && <Span>Max length exceeded</Span>}
-            </small>
-          </li>
-          <li>
-          <input type="submit" title="로그인" value="로그인" onClick={ e => handleSubmit(e)}/>
-          </li>
-      </ul>
-  </form>
-</div>
+            <li>
+                <label>비밀 번호 : </label>
+                <input type="password" id="password" 
+                    aria-invalid={errors.name ? "true" : "false"}
+                    {...register('password', { required: true, maxLength: 30 })}
+                    size="10" minlength="1" maxlength="15"/>
+                {errors.password && errors.password.type === "required" && (
+                    <Span role="alert">비밀 번호는 필수값입니다</Span>
+                )}
+                {errors.password && errors.password.type === "maxLength" && (
+                    <Span role="alert">비밀 번호는 1자에서 15 글자이어야 합니다.</Span>
+                )}
+            </li>
+        </ul>
+        <input type="submit" value="로그인"/> 
+    </form>
+    </div>
   );
 }
-
 const Span = styled.span`
-    color: red;
-    font-weight: bold;
+    color: red
 `
